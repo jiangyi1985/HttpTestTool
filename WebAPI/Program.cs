82b7fef06fa20aca75aac2bf1c8d782e209b71e8
Ivan Jiang <jiangyi_1985@hotmail.com>
2025-03-15 19:31:41 +0800

    fix vulnerability

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -6,8 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddApplicationInsightsTelemetry();
-builder.Services.AddApplicationInsightsTelemetry();
+//builder.Services.AddApplicationInsightsTelemetry();
+//builder.Services.AddApplicationInsightsTelemetry();
 
 var app = builder.Build();
 
